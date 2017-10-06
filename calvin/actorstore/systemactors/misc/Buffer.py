a6b3fd829466be0c826cc989412796059a4d6c8f
Ola Angelsmark <ola.angelsmark@ericsson.com>
2017-10-06 08:09:59 +0200

    Buffer: Move to new-style calvinsys/lib & rewrite

--- calvin/actorstore/systemactors/misc/Buffer.py
+++ calvin/actorstore/systemactors/misc/Buffer.py
@@ -14,143 +14,109 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from calvin.actor.actor import Actor, manage, condition, stateguard
-from calvin.utilities.calvinlogger import get_logger
+from calvin.actor.actor import Actor, manage, condition, stateguard, calvinlib, calvinsys
+from calvin.utilities.calvinlogger import get_actor_logger
 
-_log = get_logger(__name__)
+_log = get_actor_logger(__name__)
 
 
 class Buffer(Actor):
     """
-    Buffer data to file if necessary.
+    Buffer data to file (when necessary.)
     Inputs:
       data : some data
 
     Outputs:
-        data : the same data
+        data : the same data, eventually
     """
     
-    @manage(['num_tokens', 'buffer_name', 'sent', 'received', 'quiet'])
-    def init(self, num_tokens, buffer_name, quiet=False):
-        self.num_tokens = num_tokens
+    @manage(["buffer_name", "buffer_limit", "received", "sent", "interval"])
+    def init(self, buffer_name, buffer_limit, interval):
         self.buffer_name = buffer_name
+        self.buffer_limit = buffer_limit
+        self.interval = interval
         self.received = 0
         self.sent = 0
-        self.quiet = quiet
+        self.num_stored = 0
         self.setup()
-
-    def setup(self):
-        self.uses_external = True # always check for disk data
-        self.use(Buffer.requires[0], shorthand="collections")
-        self.use(Buffer.requires[1], shorthand="filequeue")
-        self.use(Buffer.requires[2], shorthand="json")
-        self.use(Buffer.requires[3], shorthand="timer")
-        self.buffer = self['collections'].deque()
-        
-        if self.quiet :
-            self.timer = self['timer'].once(0) # show a bit of log at the start
-            self.logger = _log.debug
-        else :
-            self.timer = None
-            self.logger = _log.info
     
+    def setup(self):
+        self.json = calvinlib.use("json")
+        queuelib = calvinlib.use("queue")
+        self.incoming = queuelib.new()
+        self.outgoing = queuelib.new()
+        self.uses_external = True
+        # reset logging
+        self.timer = calvinsys.open(self, "sys.timer.once")
+        calvinsys.write(self.timer, 10) # Wait a while
+
     def did_migrate(self):
         self.setup()
-
-    def will_migrate(self):
-        # probably not a good idea unless buffer (incl disk) is empty
-        pass
         
-    def will_end(self):
-        # write buffer to disk, then exit
-        self.logger("flushing buffers ({} items)".format(len(self.buffer)))
-        if len(self.buffer) > 0:
-            fifo = None
-            try:
-                fifo = self['filequeue'].fifo(self.buffer_name)
-                while len(self.buffer) > 0:
-                    data = self.buffer.popleft()
-                    fifo.push(self['json'].dumps(data))
-                fifo.close()
-                fifo = None
-            finally:
-                if fifo: fifo.close()
-        self.logger("done")
-    
-    
-    def incoming(self):
-        self.received += 1
-        if self.received % (self.num_tokens/2) == 0:
-            self.logger("received: {}, sent: {}, memory buffer: {}".format(self.received, self.sent, len(self.buffer)))
-    
-    def outgoing(self):
-        self.sent += 1
-        if self.sent % (self.num_tokens/2) == 0:
-            self.logger("received: {}, sent: {}, memory buffer: {}".format(self.received, self.sent, len(self.buffer)))
-
-
-    @stateguard(lambda actor: actor.timer and actor.timer.triggered)
-    @condition()
-    def flip_logging(self):
-        self.timer.ack()
-        if self.quiet:
-            self.quiet = False
-            self.logger = _log.info
-            self.timer = self['timer'].once(60)
-        else :
-            self.quiet = True
-            self.logger = _log.debug
-            self.timer = self['timer'].once(600)
-
-        
-    @stateguard(lambda actor: len(actor.buffer) == 0 and not actor.uses_external)
-    @condition(['data'], ['data'])
-    def passthrough(self, data):
-        self.incoming()
-        self.outgoing()
-        return (data, )
-
-    @condition(['data'], [])
-    def buffer_data(self, data):
-        self.incoming()
-        self.buffer.append(data)
-        if len(self.buffer) > 3*self.num_tokens:
-            fifo = None
-            try:
-                fifo = self['filequeue'].fifo(self.buffer_name)
-                data = []
-                while len(self.buffer) > self.num_tokens:
-                    data = self.buffer.popleft()
-                    fifo.push(self['json'].dumps(data))
-                self.uses_external = True
-            finally:
-                if fifo:
-                    self.logger("buffer to file -  {} items total stored".format(len(fifo)))
-                    fifo.close()
-            
-
-    @stateguard(lambda actor: len(actor.buffer) > 0)
-    @condition([], ['data'])
-    def send_buffer(self):
-        self.outgoing()
-        return (self.buffer.popleft(),)
-
-    @stateguard(lambda actor: actor.uses_external and len(actor.buffer) < actor.num_tokens)
-    @condition([], [])
-    def read_buffer(self):
+    def buffer_to_disk(self):
         fifo = None
         try:
-            fifo = self['filequeue'].fifo(self.buffer_name)
-            while len(fifo) and len(self.buffer) < 2*self.num_tokens:
-                data = fifo.pop()
-                self.buffer.append(self['json'].loads(data))
-            if len(fifo) == 0:
-                self.uses_external = False
+            queuelib = calvinlib.use("filequeue")
+            fifo = queuelib.new(self.buffer_name)
+            self.uses_external = True
+            while len(self.incoming) > 0:
+                data = self.incoming.pop()
+                fifo.push(self.json.tostring(data))
+        except Exception as e:
+            _log.info("Error buffering to disk: {}".format(e))
         finally:
             if fifo: 
-                self.logger("disk to buffer - {} items in buffer, {} total stored".format(len(self.buffer), len(fifo)))
+                self.num_stored = len(fifo)
                 fifo.close()
 
-    action_priority = (flip_logging, send_buffer, read_buffer, passthrough, buffer_data)
+    def disk_to_buffer(self):
+        fifo = None
+        try:
+            fifo = calvinlib.use("filequeue").new(self.buffer_name)
+            while len(fifo) and len(self.outgoing) < 2*self.buffer_limit:
+                data = fifo.pop()
+                self.outgoing.appendleft(self.json.fromstring(data))
+            if not len(fifo):
+                self.uses_external = False
+        except Exception as e:
+            _log.info("Error reading from disk: {}".format(e))
+        finally:
+            if fifo: 
+                self.num_stored = len(fifo)
+                fifo.close()
+
+    @stateguard(lambda self: calvinsys.can_read(self.timer))
+    @condition([], [])
+    def logger(self):
+        calvinsys.read(self.timer)
+        _log.info("received: {}, sent: {}, incoming: {}, outgoing: {}, stored: {}".format(
+            self.received, self.sent, len(self.incoming), len(self.outgoing), self.num_stored))
+        calvinsys.write(self.timer, self.interval)
     
-    requires = ['calvinsys.native.python-collections', 'calvinsys.native.python-filequeue', 'calvinsys.native.python-json', 'calvinsys.events.timer']
+    @condition(['data'], [])
+    def receive(self, data):
+        self.received += 1
+        self.incoming.appendleft(data)
+
+    @stateguard(lambda self: len(self.incoming) > 0 or self.uses_external)
+    @condition([], [])
+    def buffer_data(self):
+        if len(self.outgoing) < self.buffer_limit:
+            if not self.uses_external:
+                self.outgoing.appendleft(self.incoming.pop())
+            else:
+                self.buffer_to_disk()
+                self.disk_to_buffer()
+        elif len(self.outgoing) >= self.buffer_limit:
+            self.buffer_to_disk()
+
+    @stateguard(lambda self: len(self.outgoing) > 0)
+    @condition([], ['data'])
+    def send(self):
+        self.sent += 1
+        data = self.outgoing.pop()
+        return (data,)
+
+    action_priority = (logger, send, receive, buffer_data)
+    
+    requires = ['filequeue', 'queue', 'json', 'sys.timer.once']
